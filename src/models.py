b5d37cd1141dbd140ef368742048cd1372c62492
Ignacio Pinto <51886210+Pinto2291@users.noreply.github.com>
2026-03-31 02:35:22 +0000

    tablas creadas Planet, Character, favorites

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, ForeignKey
 
 db = SQLAlchemy()
 
@@ -17,3 +17,23 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(40), unique=True, nullable=False)
+    climate: Mapped[str] = mapped_column(String(50), nullable=False)
+    population: Mapped[int] = mapped_column(nullable=False)
+
+class Character(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    gender: Mapped[str] = mapped_column(String(20), nullable=False)
+    age: Mapped[int] = mapped_column(nullable=False)
+    race: Mapped[str] = mapped_column(String(50), nullable=False)
+    homeworld: Mapped[str] = mapped_column(String(50), nullable=False)
+
+class Favorite(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planet.id"), nullable=True)
+    character_id: Mapped[int] = mapped_column(ForeignKey("character.id"), nullable=True)
